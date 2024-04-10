a2214bf97f92ebd5f9dbcbd3e76915ff5d77e215
Artur <artur.spiewak2967@gmail.com>
2024-04-10 22:53:18 +0200

    Add Encrypt Frame - not finished

--- frames/AboutUsFrame.py
+++ frames/AboutUsFrame.py
@@ -5,5 +5,12 @@ class AboutUsFrame(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         tk.Label(self, text="About us").pack(pady=10)
+        description = "This is an application that allows you to:\n" \
+                        " sign, verify, encrypt and decrypt documents\n" \
+                        "using a qualified electronic signature.\n" \
+                        "Authors: \n" \
+                        "Artur Śpiewak - 188663\n" \
+                        "Przemysław Szumczyk - 188956\n"
+        tk.Label(self, text=description).pack(pady=10)
         tk.Button(self, text="Main menu", command=lambda: controller.show_frame("MainFrame")).pack()
         
--- frames/EncryptFrame.py
+++ frames/EncryptFrame.py
@@ -1,9 +1,25 @@
 import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
 class EncryptFrame(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.filename = ""
+        self.keyname = ""
         tk.Label(self, text="Encrypt").pack(pady=10)
-        tk.Button(self, text="Main menu", command=lambda: controller.show_frame("MainFrame")).pack()
+        tk.Button(self, text="Select file", command=self._select_file).pack(fill=tk.X, padx=50, pady=5)
+        tk.Label(self, text="File path:\n" + self.filename).pack()
+        tk.Button(self, text="Select key", command=self._select_key).pack(fill=tk.X, padx=50, pady=5)
+        tk.Label(self, text="Key path:\n" + self.keyname).pack()
+        tk.Button(self, text="Main menu", command=lambda: controller.show_frame("MainFrame")).pack(fill=tk.X, padx=50, pady=5)
         
+    def _select_file(self):
+        self.filename = filedialog.askopenfilename()
+        
+    def _select_key(self):
+        self.keyname = filedialog.askopenfilename()
+        
+    def _check_format(self, formatlist):
+        pass
