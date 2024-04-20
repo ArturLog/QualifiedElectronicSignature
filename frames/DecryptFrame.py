21ec9eeb7c62436cb1a763186da67d1efb0a0a1e
Artur <artur.spiewak2967@gmail.com>
2024-04-21 01:19:24 +0200

    Decrypt work with pin

--- frames/DecryptFrame.py
+++ frames/DecryptFrame.py
@@ -9,7 +9,7 @@ class DecryptFrame(tk.Frame):
         self.controller = controller
         self.key_path = None
         self.file_path = None
-        self.pin = "1234"
+        self.pin = None
         self.filename = tk.StringVar()
         self.keyname = tk.StringVar()
         tk.Label(self, text="Decrypt").pack(pady=10)
@@ -19,6 +19,10 @@ class DecryptFrame(tk.Frame):
         
         tk.Button(self, text="Find device", command=self._find_device).pack(fill=tk.X, padx=50, pady=5)
         tk.Label(self, textvariable=self.keyname).pack()
+        
+        tk.Label(self, textvariable="Enter the PIN").pack()
+        self.pin_entry = tk.Entry(self, show="*", width=4)
+        self.pin_entry.pack(pady=5)
         
         tk.Button(self, text="Decrypt", command=self._decrypt_controller).pack(fill=tk.X, padx=50, pady=5)
         tk.Button(self, text="Main menu", command=lambda: controller.show_frame("MainFrame")).pack(fill=tk.X, padx=50, pady=5)
@@ -39,18 +43,28 @@ class DecryptFrame(tk.Frame):
             messagebox.showerror("Error", "No external storage found")
             
     def _decrypt_controller(self):
-        if self.key_path and self.file_path:
+        if self.key_path and self.file_path and self._check_pin():
             self._decrypt()
             messagebox.showinfo("Info", "File decrypted successfully")
             self._clear_paths()
         else:
-            messagebox.showerror("Error", "Select file and put pendrive with private key first")    
+            messagebox.showerror("Error", "Select file, type PIN and put pendrive with private key first")    
+    
+    def _check_pin(self):
+        self.pin = self.pin_entry.get()
+        if not self.pin:
+            messagebox.showerror("Error", "Type PIN first")
+            return False
+        elif len(self.pin) != 4 or not self.pin.isdigit():
+            messagebox.showerror("Error", "PIN must be exactly 4 digits")
+            return False
+        return True
     
     def _decrypt(self):
         with open(self.file_path, "rb") as file:
             file_content = file.read()
         
-        private_key = self.controller.decrypt_private_key(self.pin, self.key_path)
+        private_key = self.controller.decrypt_private_key(self.pin, self.key_path) # TODO BAD PIN
             
         cipher = PKCS1_OAEP.new(private_key)
         decrypted_content = cipher.decrypt(file_content)
